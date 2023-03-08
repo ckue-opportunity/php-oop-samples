9a4df0fb03441f9d7cb5cbd665d2da5d181474c4
ckue-opportunity <ckue.opportunity@gmail.com>
2023-03-08 17:06:33 +0100

    class Fruit (live coding)

--- copies/fruit copy.php
+++ copies/fruit copy.php
@@ -1,23 +1,20 @@
 <?php
 
-// PHASE 1: declare class ---------------------------------
-
 class Fruit {
     protected string $name;
     public string $color;
 
-    // constructor: both arguments are defined as optional
+    // constructor
     function __construct(string $name = '', string $color = '') {
         $this->name = $name;
         $this->color = $color;
     }
 
-    // setter function for $name
-    function set_name($n) {
-        $this->name = $n;
+    // getter and setter methods for $name
+    function set_name($name) {
+        $this->name = $name;
     }
 
-    // getter function for $name
     function get_name() {
         return $this->name;
     }
--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
     require "./includes/fruit.php";
 
-    $fruit1 = new Fruit(); // lazy: $fruit = new Fruit;
-    $fruit1->set_name('Banana');
+    // PHASE 2: generate instances ---------------------------------
 
-    $fruit2 = new Fruit('Apple', 'red-orange');
+    // initialize fruit instances
+    $fruit1 = new Fruit(); // short: new Fruit;
+    $fruit1->set_name('Banana');
+    $fruit1->color = 'green';
+
+    $fruit2 = new Fruit('Apple', 'orange-red');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,13 +21,25 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <!-- Test Output -->
     <div class="container" style="padding: 20px;">
         <h1>class Fruit</h1>
-        <p><?php echo '$fruit1->get_name() = ' . $fruit1->get_name(); ?><p>
-        <p><?php echo '$fruit2->get_name() = ' . $fruit2->get_name() . '<br/>' . 
-                      '$fruit2->color = ' . $fruit2->color; ?><p>
-    </div>
+        <p><?php 
+            // PHASE 3: display variables and call methods -----------------
+            echo '$fruit1->get_name() = ' . $fruit1->get_name() . '<br/>' .
+                 '$fruit1->color = ' . $fruit1->color;
 
+            echo '<br/><br/>';
+
+            echo '$fruit2->get_name() = ' . $fruit2->get_name() . '<br/>' .
+                 '$fruit2->color = ' . $fruit2->color;
+            
+            echo '<br/><br/>';
+            
+            var_dump($fruit1);
+            echo '<br/><br/>';
+            print_r($fruit2);
+           ?><p>
+        
+    </div>
 </body>
 </html>
